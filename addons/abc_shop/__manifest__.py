12cf9091d842b301f0b26a7ac8e390be1108ac5b
Saraju Palukasi <saraju@greatbear.tech>
2017-12-05 22:44:02 +0545

    initialize a addon abc_shop

---
+++ addons/abc_shop/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name':'ABC Shop Invoice',
+    'description':'Invoicing system for the ABC Shop.',
+    'author':'Saraju Palukasi',
+    'version':'10.0.0.1',
+    'category':'Uncategorized',
+    'depends':[
+        'base',
+    ],
+    'application':True,
+
+}
