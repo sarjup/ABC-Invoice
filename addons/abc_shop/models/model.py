b563474f2e14400957a42e73acbb090fd6512944
Saraju Palukasi <saraju@greatbear.tech>
2017-12-05 23:31:50 +0545

    add model for product, customer and vendor

---
+++ addons/abc_shop/models/model.py
@@ -0,0 +1,38 @@
+from odoo import models, fields
+
+class Customers(models.Model):
+    _name = "abc.customers"
+    _description = "This model is for customers of ABC shop"
+
+    name = fields.Char(string="Name", required = True)
+    address = fields.Char(string = "Address")
+    email = fields.Char(string = "Email")
+    contact = fields.Char(string="Phone", required = True)
+    active = fields.Boolean(string = "Active?", default = True)
+
+
+class Vendors(models.Model):
+    _name = "abc.vendors"
+    _description = "this model is for vendors of ABC Shop"
+
+    name = fields.Char(string = "Name", required =True)
+    address = fields.Char(string = "Address")
+    email = fields.Char(string = "Email")
+    contact = fields.Char(string="Phone", required = True)
+    active = fields.Boolean(string = "Active?", default = True)
+
+
+class Products(models.Model):
+    _name = "abc.products"
+    _description = "this model specifies the products for the ABC Shop"
+
+    name = fields.Char(string = "Product Name", required = True)
+    type = fields.Selection([
+        ('consumable','Consumable'),
+        ('service','Service')
+    ],
+    string = 'Product Type',
+    default = 'consumable')
+    list_price = fields.Float(string = "Sale Price")
+    standard_price = fields.Float(string="Cost Price")
+    active = fields.Boolean(string = "Active?", default = True)
